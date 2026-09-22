9cfa7ec13bcfd6b65f2e055e08d26640517eb0fc
Roseline001 <roseline.jonathan@student.moringaschool.com>
2026-09-22 20:43:09 +0300

    condition & loop statements

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+// if-else statement to check the temperature
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+// if-else statement to check the timeOfDay
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+while (soilMoisture <= 40) {
+    console.log(soilMoisture);
+    soilMoisture = soilMoisture + 5;
+}
